63a2d2c21a12de924e57a73737b429a51a55ef85
میلاد قلی پور نوسر <57814334+miladgpn92@users.noreply.github.com>
2025-10-07 11:26:48 +0330

    add gym

---
+++ Entities/Gym/Gym.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Gym : BaseWithSeoEntity
+    {
+        public string Title { get; set; }
+
+        public string Address { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public string Slug { get; set; }
+    }
+}
